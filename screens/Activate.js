440a49a4d83274d69a99342adf4cac3c4e1e0706
Dereck Quock <djquock@gmail.com>
2020-03-15 19:30:30 -0700

    upgrade react-navigation to v5 and refactor navigation (#77)
    * upgrade react-navigation to v5 and refactor navigation
    
    * remove `utils/header.js`
    
    Co-authored-by: dereck <dquock@paypal.com>

--- screens/Activate.js
+++ screens/Activate.js
@@ -1,66 +1,57 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, Image } from 'react-native';
-import { useSafeArea } from 'react-native-safe-area-context';
+import { HeaderHeightContext } from '@react-navigation/stack';
 import * as WebBrowser from 'expo-web-browser';
 import * as Amplitude from 'expo-analytics-amplitude';
 import Colors from '../constants/Colors';
-import { getHeaderInset } from '../utils/header';
 import Text from '../components/shared/Text';
 import Button from '../components/shared/Button';
 
 const ActivateScreen = () => {
-  const insets = useSafeArea();
-
   return (
-    <ScrollView
-      style={[styles.mainContainer, { paddingTop: insets.top }]}
-      {...getHeaderInset()}
-    >
-      <Image
-        source={require('../assets/images/activate.png')}
-        style={styles.image}
-      />
-      <View style={[styles.container, { paddingBottom: insets.bottom + 16 }]}>
-        <Text style={styles.heading}>Your next steps</Text>
-        <Text style={styles.content}>
-          Activate is about you and the environments we design at Echo.Church
-          that can lead you to discover your purpose, develop friendships, and
-          make a difference. Activate is a free dinner experience where we can
-          get to know you and give you a glimpse of what being connected at
-          Echo.Church is all about.
-        </Text>
+    <HeaderHeightContext.Consumer>
+      {headerHeight => (
+        <ScrollView
+          style={[styles.mainContainer, { paddingTop: headerHeight }]}
+        >
+          <Image
+            source={require('../assets/images/activate.png')}
+            style={styles.image}
+          />
+          <View style={[styles.container, { paddingBottom: headerHeight }]}>
+            <Text style={styles.heading}>Your next steps</Text>
+            <Text style={styles.content}>
+              Activate is about you and the environments we design at
+              Echo.Church that can lead you to discover your purpose, develop
+              friendships, and make a difference. Activate is a free dinner
+              experience where we can get to know you and give you a glimpse of
+              what being connected at Echo.Church is all about.
+            </Text>
 
-        <Button
-          title="RSVP Here"
-          style={styles.button}
-          onPress={() => {
-            Amplitude.logEvent('TAP Activate RSVP');
-            WebBrowser.openBrowserAsync(
-              'https://docs.google.com/forms/d/e/1FAIpQLSdZ1EBI_kCqt8xtK1n1PBfcBUlHFPl45o-9Ls3O2srwejpjGw/viewform?vc=0&c=0&w=1',
-              { toolbarColor: Colors.darkestGray }
-            ).catch(err => {
-              Amplitude.logEventWithProperties('ERROR with WebBrowser', {
-                error: err,
-              });
-              WebBrowser.dismissBrowser();
-            });
-          }}
-        />
-      </View>
-    </ScrollView>
+            <Button
+              title="RSVP Here"
+              style={styles.button}
+              onPress={() => {
+                Amplitude.logEvent('TAP Activate RSVP');
+                WebBrowser.openBrowserAsync(
+                  'https://docs.google.com/forms/d/e/1FAIpQLSdZ1EBI_kCqt8xtK1n1PBfcBUlHFPl45o-9Ls3O2srwejpjGw/viewform?vc=0&c=0&w=1',
+                  { toolbarColor: Colors.darkestGray }
+                ).catch(err => {
+                  Amplitude.logEventWithProperties('ERROR with WebBrowser', {
+                    error: err,
+                  });
+                  WebBrowser.dismissBrowser();
+                });
+              }}
+            />
+          </View>
+        </ScrollView>
+      )}
+    </HeaderHeightContext.Consumer>
   );
 };
 
-ActivateScreen.navigationOptions = {
-  headerTitle: () => <Text style={styles.header}>Activate</Text>,
-};
-
 const styles = StyleSheet.create({
-  header: {
-    fontFamily: 'NunitoSans-Regular',
-    fontSize: 26,
-    color: Colors.white,
-  },
   mainContainer: {
     flex: 1,
     backgroundColor: Colors.darkestGray,
---
+++ utils/useHandleTabChange.js
@@ -0,0 +1,18 @@
+/**
+ * Custom hook to handle tab changes by getting if a
+ * tab is focused. Call this hook from each main tab.
+ */
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import * as Amplitude from 'expo-analytics-amplitude';
+
+export default tab => {
+  useFocusEffect(
+    React.useCallback(() => {
+      Amplitude.logEvent(`PAGEVIEW ${tab} tab`);
+      StatusBar.setHidden(tab === 'Home', 'fade');
+    }, [])
+  );
+};
