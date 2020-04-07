563aacb73f9045b37edf2c8fbae12a6a5e4be783
dereck <dquock@paypal.com>
2020-04-07 00:10:05 -0700

    remove unnecessary build files

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "echo://10.0.0.168:19000";
-
-}
