90f498d99fd812dab01686b65b24aa74ab2a8204
dereck <dquock@paypal.com>
2020-04-15 23:24:14 -0700

    add helper button to enable Storybook

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,19 +4,19 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../components/LiveCard.stories');
-  require('../components/shared/Button.stories');
-  require('../components/shared/Checkbox.stories');
-  require('../components/shared/Spinner.stories');
-  require('../components/shared/Typography/index.stories');
+	require('../components/LiveCard.stories');
+	require('../components/shared/Button.stories');
+	require('../components/shared/Checkbox.stories');
+	require('../components/shared/Spinner.stories');
+	require('../components/shared/Typography/index.stories');
 }
 
 const stories = [
-  '../components/LiveCard.stories',
-  '../components/shared/Button.stories',
-  '../components/shared/Checkbox.stories',
-  '../components/shared/Spinner.stories',
-  '../components/shared/Typography/index.stories',
+	'../components/LiveCard.stories',
+	'../components/shared/Button.stories',
+	'../components/shared/Checkbox.stories',
+	'../components/shared/Spinner.stories',
+	'../components/shared/Typography/index.stories'
 ];
 
 module.exports = {
