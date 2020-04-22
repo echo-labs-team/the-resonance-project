78ea8d4260b39dd3d88077c06b2a29df25bdb040
dereck <dquock@paypal.com>
2020-04-22 00:36:12 -0700

    fix android issues

--- android/app/src/main/java/com/echo/church/app/MainActivity.java
+++ android/app/src/main/java/com/echo/church/app/MainActivity.java
@@ -6,9 +6,19 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.res.Configuration;
+import expo.modules.splashscreen.SplashScreen;
+import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // SplashScreen.show(...) has to be called after super.onCreate(...)
+        SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class);
+    }
 
     // Added automatically by Expo Config
     @Override
