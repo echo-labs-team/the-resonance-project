d6dee4975c02bfb38c976b1f65ccdc1e5f83db21
Dereck Quock <djquock@gmail.com>
2020-06-18 18:32:18 -0700

    add featured card for the hope project (#260)
    * add featured card for the hope project
    
    * fetch images instead
    
    * skrape data from wordpress 👨‍💻
    
    * posts should be in decending order
    
    Co-authored-by: dereck <dquock@paypal.com>

---
+++ screens/Featured.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import { HeaderHeightContext } from '@react-navigation/stack';
+import * as WebBrowser from 'expo-web-browser';
+import * as Amplitude from 'expo-analytics-amplitude';
+import { Feather } from '@expo/vector-icons';
+import Colors from '../constants/Colors';
+import Layout from '../constants/Layout';
+import { Text } from '../components/shared/Typography';
+import Button from '../components/shared/Button';
+
+function FeaturedScreen({ navigation }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    StatusBar.setHidden(false);
+
+    // set the post data for the list up to today's date
+    const items = [];
+    const start = new Date(2020, 5, 15);
+    const today = new Date();
+    const timeDiff = today.getTime() - start.getTime();
+    const dayDiff = Math.round(timeDiff / (1000 * 3600 * 24));
+
+    for (let i = 0; i < dayDiff; i++) {
+      const postDate = new Date(start);
+      const day = i + 1;
+
+      postDate.setDate(start.getDate() + i);
+      items.push({
+        value: `DAY ${day} - ${postDate.toLocaleDateString()}`,
+        slug: `day${day}-hopeproject`,
+      });
+    }
+
+    setPosts(items.reverse());
+  }, []);
+
+  return (
+    <HeaderHeightContext.Consumer>
+      {(headerHeight) => (
+        <View style={[styles.mainContainer, { paddingTop: headerHeight }]}>
+          <ImageBackground
+            progressiveRenderingEnabled
+            source={{
+              uri:
+                'https://echo.church/wp-content/uploads/2020/05/Hope-Project_TitleSlideFinal111.jpg',
+            }}
+            style={styles.backgroundImage}
+          />
+          <Image
+            progressiveRenderingEnabled
+            source={{
+              uri:
+                'https://echo.church/wp-content/uploads/elementor/thumbs/HP_WhiteIcon-1-opizmnxv1bqwwsj6ih2c78mebgu69hsmb89bs0b45s.png',
+            }}
+            style={styles.logo}
+          />
+          <FlatList
+            keyExtractor={({ value }) => value}
+            data={posts}
+            ItemSeparatorComponent={({ highlighted }) => (
+              <View
+                style={[styles.separator, highlighted && { marginLeft: 0 }]}
+              />
+            )}
+            renderItem={({ item = {} }) => (
+              <TouchableHighlight
+                underlayColor="transparent"
+                onPress={() => {
+                  Amplitude.logEvent(`OPEN ${item.value}`);
+                  navigation.navigate('FeaturedDetails', item);
+                }}
+              >
+                <View style={styles.item}>
+                  <Text L bold style={styles.text}>
+                    {item.value}
+                  </Text>
+                  <Feather
+                    name={'chevron-right'}
+                    size={30}
+                    color={Colors.white}
+                  />
+                </View>
+              </TouchableHighlight>
+            )}
+            style={styles.list}
+          />
+          <Button
+            title="Join the Hope Project"
+            style={styles.button}
+            onPress={() => {
+              WebBrowser.openBrowserAsync('https://echo.church/hopeproject/', {
+                toolbarColor: Colors.darkestGray,
+              }).catch((err) => {
+                Amplitude.logEventWithProperties('ERROR with WebBrowser', {
+                  error: err,
+                });
+                WebBrowser.dismissBrowser();
+              });
+            }}
+          />
+        </View>
+      )}
+    </HeaderHeightContext.Consumer>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: Colors.darkestGray,
+  },
+  backgroundImage: {
+    width: '100%',
+    height: Layout.window.height,
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    opacity: 0.5,
+    backgroundColor: Colors.blue,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginTop: 6,
+    alignSelf: 'center',
+  },
+  list: {
+    paddingTop: 4,
+    paddingBottom: 20,
+  },
+  separator: {
+    height: 0.5,
+    marginLeft: 20,
+    backgroundColor: Colors.blue,
+  },
+  item: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  text: {
+    paddingVertical: 10,
+    paddingLeft: 8,
+    color: Colors.white,
+  },
+  button: { marginVertical: 10, marginHorizontal: 10 },
+});
+
+export default FeaturedScreen;
--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Image,
-  Linking,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  TouchableHighlight,
-  View,
-} from 'react-native';
+import { RefreshControl, ScrollView, StyleSheet, View } from 'react-native';
 import { useSafeArea } from 'react-native-safe-area-context';
 import { useScrollToTop } from '@react-navigation/native';
 import * as Amplitude from 'expo-analytics-amplitude';
-import * as WebBrowser from 'expo-web-browser';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Layout from '../constants/Layout';
 import Colors from '../constants/Colors';
 import { getInstagramPosts } from '../data/instagram';
 import { getBlogPosts } from '../data/blogPosts';
@@ -25,6 +14,8 @@ import Button from '../components/shared/Button';
 import Spinner from '../components/shared/Spinner';
 import EchoLogo from '../components/EchoLogo';
 import HomeCardPlaceholder from '../components/HomeCardPlaceholder';
+import FeaturedCard from '../components/FeaturedCard';
+import Card from '../components/HomeCard';
 
 const sortPosts = (firstPost = {}, secondPost = {}) => {
   const { date: firstDate, type: firstType } = firstPost;
@@ -40,102 +31,6 @@ const sortPosts = (firstPost = {}, secondPost = {}) => {
   }
 
   return new Date(secondDate) - new Date(firstDate);
-};
-
-function getIcon(type) {
-  if (type === 'INSTAGRAM') {
-    return {
-      uri:
-        'https://www.instagram.com/static/images/ico/apple-touch-icon-76x76-precomposed.png/4272e394f5ad.png',
-    };
-  }
-
-  return {
-    BLOG: require('../assets/icons/Blog.png'),
-    EVENTS: require('../assets/icons/Events.png'),
-    ANNOUNCEMENTS: require('../assets/icons/Announcements.png'),
-    'VERSE OF THE DAY': {
-      expoIcon: (
-        <MaterialCommunityIcons name={'bible'} size={24} color={Colors.white} />
-      ),
-    },
-  }[type];
-}
-
-function getImageHeight(type, image) {
-  // if we don't have an image, let's not take up space for it
-  if (!image) {
-    return 0;
-  }
-
-  // these images are pretty large
-  if (type === 'INSTAGRAM' || type === 'VERSE OF THE DAY') {
-    return Layout.window.width - 20;
-  }
-
-  return 200;
-}
-
-const Card = ({ type, url, image, title, date }) => {
-  const icon = getIcon(type);
-
-  return (
-    <TouchableHighlight
-      underlayColor={Colors.darkBlue}
-      style={styles.card}
-      onPress={() => {
-        Amplitude.logEventWithProperties('TAP post', {
-          post_type: type.toLowerCase(),
-        });
-
-        if (type === 'BLOG') {
-          return WebBrowser.openBrowserAsync(url, {
-            toolbarColor: Colors.darkestGray,
-          }).catch((err) => {
-            Amplitude.logEventWithProperties('ERROR with WebBrowser', {
-              error: err,
-            });
-            WebBrowser.dismissBrowser();
-          });
-        }
-        Linking.openURL(url);
-      }}
-    >
-      <View>
-        <Image
-          source={{ uri: image }}
-          style={[
-            styles.image,
-            {
-              height: getImageHeight(type, image),
-            },
-          ]}
-        />
-        <View style={styles.cardTypeView}>
-          <View style={styles.cardType}>
-            {icon.expoIcon ? (
-              icon.expoIcon
-            ) : (
-              <Image source={icon} style={styles.cardTypeIcon} />
-            )}
-            <Text bold style={styles.cardTypeText}>
-              {type}
-            </Text>
-          </View>
-          {date && (
-            <Text light style={styles.cardTypeText}>
-              {date}
-            </Text>
-          )}
-        </View>
-        {title && (
-          <Text style={styles.title} numberOfLines={3}>
-            {title}
-          </Text>
-        )}
-      </View>
-    </TouchableHighlight>
-  );
 };
 
 const HomeScreen = () => {
@@ -158,7 +53,6 @@ const HomeScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
   const [tryAgain, setTryAgain] = useState(false);
 
-  // fetch data on mount
   useEffect(() => {
     const getPosts = async () => {
       const igPosts = (await getInstagramPosts()) || [];
@@ -220,6 +114,8 @@ const HomeScreen = () => {
           </Text>
         </AnimateChildrenIn>
 
+        <FeaturedCard />
+
         {cardData.length ? (
           cardData.map((item, index) => {
             if (item?.url?.includes('loading')) {
@@ -263,40 +159,6 @@ const styles = StyleSheet.create({
   logo: {
     marginLeft: 10,
     color: Colors.white,
-  },
-  card: {
-    flex: 1,
-    marginBottom: 16,
-    backgroundColor: Colors.darkestGray,
-    borderRadius: 8,
-    overflow: 'hidden',
-  },
-  image: {
-    width: undefined,
-    resizeMode: 'cover',
-  },
-  title: {
-    paddingHorizontal: 8,
-    paddingVertical: 8,
-  },
-  cardTypeView: {
-    paddingTop: 16,
-    paddingHorizontal: 8,
-    paddingBottom: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  cardType: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  cardTypeIcon: {
-    width: 16,
-    height: 16,
-  },
-  cardTypeText: {
-    paddingLeft: 8,
   },
   error: { marginBottom: 10 },
 });
