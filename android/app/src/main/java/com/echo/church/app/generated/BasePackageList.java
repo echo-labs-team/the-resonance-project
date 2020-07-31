5a6e70130e25177cb5fbc667be550fea79b71fb1
Phillip Trent <philliphtrent@gmail.com>
2020-07-30 21:06:54 -0700

    removed hope project stuff and did another push

--- android/app/src/main/java/com/echo/church/app/generated/BasePackageList.java
+++ android/app/src/main/java/com/echo/church/app/generated/BasePackageList.java
@@ -7,7 +7,6 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.analytics.amplitude.AmplitudePackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
